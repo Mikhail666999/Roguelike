b401d6db6fb1076840a2d7318560ab9825850c35
Mika <61342907+Mikhail666999@users.noreply.github.com>
2023-11-03 11:12:44 +0500

    Add files via upload

---
+++ 1_compile.bat
@@ -0,0 +1,15 @@
+g++ -g -c main.cpp -o bin\main.o -I%cd%\lib\64\mingw64\include
+@if %errorlevel% == 0 goto :nextone
+@goto :nextpause
+
+:nextone
+g++ bin\main.o -o bin\app.exe -L%cd%\lib\64\mingw64\lib -lncursesw
+@if %errorlevel% == 0 goto :nexttwo
+@goto :nextpause
+
+:nexttwo
+exit
+
+:nextpause
+@pause
+exit()
